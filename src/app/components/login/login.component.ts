3bfcde2e2938f36eb6e51e03c91e2cba5b0bb06c
tijhemerson <jhemerson1931@gmail.com>
2022-08-03 15:09:50 -0300

    Login concluído e logout

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -19,14 +21,19 @@ export class LoginComponent implements OnInit {
   senha = new FormControl(null, Validators.minLength(3))
 
   constructor(
-    private toast: ToastrService) { }
+    private toast: ToastrService,
+    private service: AuthService,
+    private router: Router) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   logar(){
-    this.toast.error('Usuário e/ou senha inválidos!', 'Login');
-    this.creds.senha='';
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.sucessfulLogin(resposta.headers.get('Authorization').substring(7));
+      this.router.navigate(['']);
+    }, () =>{
+      this.toast.error('Usuário e/ou senha inválidos!');
+    })
   }
 
   validaCampos(): boolean{
