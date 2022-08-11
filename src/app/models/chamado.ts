302841e18299131c931efb27c5dc0f1feecc93cd
tijhemerson <jhemerson1931@gmail.com>
2022-08-11 17:00:58 -0300

    interface/service chamado

---
+++ src/app/models/chamado.ts
@@ -0,0 +1,13 @@
+export interface Chamado{
+    id?: any;
+    dataAbertura?: String;
+    dataFechamento?: String;
+    prioridade: String;
+    status: string;
+    titulo: string;
+    descricao: string;
+    tecnico: any;
+    cliente: any;
+    nomeCliente: string;
+    nomeTecnico: string;
+}
